d39c9b6e07f15d07d997cde12d3be9f7c5b95fc2
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-10-13 20:04:36 +0200

    Initial commit

---
+++ src/main/java/com/lab/CalculatorLauncher.java
@@ -0,0 +1,10 @@
+package com.lab;
+
+import java.util.Arrays;
+
+public class CalculatorLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Coming soon!");
+    }
+}
